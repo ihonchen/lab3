0776e8d20c18e6ef32776efb1e4b6cecc49ec0b7
i0c006@ucsd.edu <i0c006@ucsd.edu>
2019-01-24 09:08:00 -0800

    lab3finished

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,29 @@
-'use strict';
 
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
-
-/*
- * Function that is called when the document is ready.
- */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+  $("#buttontop").click(function(e) {
+    $('.jumbotron h1').text("Javascript has take control");
+        $("#buttontop").text("Please wait...");
+        $("#buttontop").toggleCla("active");
+  })};
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+$(document).ready(function() {
+   initializePage();
+});
+
+$("a.thumbnail").click(projectClick);
+
+
+function projectClick(e) { 
+    
+    e.preventDefault();
+    var containingProject = $(this).closest("a.thumbnail");
+var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<div class='project-description'><p>Description of the project.</p></div>");
+    }
 }
+
+
+
